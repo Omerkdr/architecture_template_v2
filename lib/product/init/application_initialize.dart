0e2719de00040056de3b9972e232bad5ba9fdc95
Ömer Kadir <omerkdr8@outlook.com>
2024-01-10 21:28:15 +0300

    initialize added

---
+++ lib/product/init/application_initialize.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_logger/easy_logger.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kartal/kartal.dart';
+import 'package:logger/logger.dart';
+
+@immutable
+
+//This class is used to initilialize the applicaton process
+final class ApplicationInitialize {
+  // project basic required initialize
+  Future<void> make() async {
+    await runZonedGuarded<Future<void>>(
+      _initialize,
+      (error, stack) {
+        Logger().e(error);
+      },
+    );
+  }
+
+  Future<void> _initialize() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await EasyLocalization.ensureInitialized();
+
+    // error message read
+    EasyLocalization.logger.enableLevels = [LevelMessages.error];
+    await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+    // TODO: Splash screen
+
+    await DeviceUtility.instance.initPackageInfo();
+
+    FlutterError.onError = (details) {
+      // crash log insert here
+      // custom service or custom logger insert here
+      // TO DO: add crashlytics
+    };
+  }
+}
