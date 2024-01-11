f142bc24cf837f4dd587c7e0c5bd51707080c4f7
Ömer Kadir <omerkdr8@outlook.com>
2024-01-11 23:40:06 +0300

    color schemes updated

--- lib/product/init/theme/custom_light_theme.dart
+++ lib/product/init/theme/custom_light_theme.dart
@@ -1,9 +1,17 @@
 import 'package:architecture_template_v2/product/init/theme/custom_color_scheme.dart';
+import 'package:architecture_template_v2/product/init/theme/custom_theme.dart';
 import 'package:flutter/material.dart';
 
-final class CustomLightTheme {
-  final ThemeData themeData = ThemeData(
-    useMaterial3: true,
-    colorScheme: CustomColorScheme.lightColorScheme,
-  );
+final class CustomLightTheme implements CustomTheme {
+  @override
+  ThemeData get themeData => ThemeData(
+        useMaterial3: true,
+        // fontFamily: GoogleFonts.roboto().fontFamily,
+        colorScheme: CustomColorScheme.lightColorScheme,
+        floatingActionButtonTheme: floatingActionButtonThemeData,
+      );
+
+  @override
+  FloatingActionButtonThemeData get floatingActionButtonThemeData =>
+      const FloatingActionButtonThemeData();
 }
