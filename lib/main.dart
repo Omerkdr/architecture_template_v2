a3b1c644bacf390d46ac7950ab5c9a8db0ea1cb7
Ömer Kadir <omerkdr8@outlook.com>
2024-01-12 14:54:53 +0300

    theme part updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:architecture_template_v2/feature/home/view/home_view.dart';
 import 'package:architecture_template_v2/product/init/application_initialize.dart';
 import 'package:architecture_template_v2/product/init/product_localization.dart';
-import 'package:architecture_template_v2/product/init/theme/custom_color_scheme.dart';
+import 'package:architecture_template_v2/product/init/theme/custom_dark_theme.dart';
+import 'package:architecture_template_v2/product/init/theme/custom_light_theme.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
@@ -16,10 +17,9 @@ class _MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          useMaterial3: true, colorScheme: CustomColorScheme.lightColorScheme),
-      darkTheme: ThemeData(
-          useMaterial3: true, colorScheme: CustomColorScheme.darkColorScheme),
+      theme: CustomLightTheme().themeData,
+      darkTheme: CustomDarkTheme().themeData,
+      themeMode: ThemeMode.light,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
--- lib/product/init/theme/custom_light_theme.dart
+++ lib/product/init/theme/custom_light_theme.dart
@@ -1,12 +1,13 @@
 import 'package:architecture_template_v2/product/init/theme/custom_color_scheme.dart';
 import 'package:architecture_template_v2/product/init/theme/custom_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 final class CustomLightTheme implements CustomTheme {
   @override
   ThemeData get themeData => ThemeData(
         useMaterial3: true,
-        fontFamily: GoogleFonts.roboto().fontFamily,
+        fontFamily: GoogleFonts.aDLaMDisplay().fontFamily,
         colorScheme: CustomColorScheme.lightColorScheme,
         floatingActionButtonTheme: floatingActionButtonThemeData,
       );
