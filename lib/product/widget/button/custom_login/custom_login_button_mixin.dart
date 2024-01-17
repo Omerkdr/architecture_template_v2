099887277d02c6539e24c5bf805520e812e696d9
Ömer Kadir <omerkdr8@outlook.com>
2024-01-17 14:24:39 +0300

    mixin, added

--- lib/product/widget/button/custom_login/custom_login_button_mixin.dart
+++ lib/product/widget/button/custom_login/custom_login_button_mixin.dart
@@ -1,26 +1,26 @@
-// part of 'custom_login_button.dart';
+part of 'custom_login_button.dart';
 
-// mixin _CustomLoginButtonMixin
-//     on MountedMixin<CustomLoginButton>, State<CustomLoginButton> {
-//   final ValueNotifier<bool> _isLoadingNotifier = ValueNotifier<bool>(false);
+mixin _CustomLoginButtonMixin
+    on MountedMixin<CustomLoginButton>, State<CustomLoginButton> {
+  final ValueNotifier<bool> _isLoadingNotifier = ValueNotifier<bool>(false);
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     _isLoadingNotifier.value = false;
-//   }
+  @override
+  void initState() {
+    super.initState();
+    _isLoadingNotifier.value = false;
+  }
 
-//   @override
-//   void dispose() {
-//     super.dispose();
-//   }
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
-//   Future<void> _onPressed(BuildContext context) async {
-//     _isLoadingNotifier.value = true;
-//     final response = await widget.onOperation.call();
-//     await safeOperation(() async {
-//       if (response) Navigator.of(context).pop();
-//       _isLoadingNotifier.value = false;
-//     });
-//   }
-// }
+  Future<void> _onPressed(BuildContext context) async {
+    _isLoadingNotifier.value = true;
+    final response = await widget.onOperation.call();
+    await safeOperation(() async {
+      if (response) Navigator.of(context).pop();
+      _isLoadingNotifier.value = false;
+    });
+  }
+}
