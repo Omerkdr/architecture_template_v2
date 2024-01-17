aa3a302182bcbb34a17e327d588ea7e754ae578d
Ömer Kadir <omerkdr8@outlook.com>
2024-01-17 13:57:02 +0300

    updated

--- lib/product/widget/button/custom_login_button.dart
+++ lib/product/widget/button/custom_login_button.dart
@@ -1,29 +1,36 @@
-import 'package:architecture_template_v2/product/widget/button/normal_button.dart';
+import 'package:architecture_template_v2/product/widget/button/index.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:widgets/widgets.dart';
 
-part 'custom_login_button_mixin.dart';
-
-final class CustomLoginButton extends StatefulWidget {
+class CustomLoginButton extends StatefulWidget {
   const CustomLoginButton({required this.onOperation, super.key});
   final AsyncValueGetter<bool> onOperation;
+
   @override
   State<CustomLoginButton> createState() => _CustomLoginButtonState();
 }
 
-class _CustomLoginButtonState extends State<CustomLoginButton>
-    with MountedMixin, _CustomLoginButtonMixin {
+final class _CustomLoginButtonState extends State<CustomLoginButton> {
+  final ValueNotifier<bool> _isLoadingNotifier = ValueNotifier<bool>(false);
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<bool>(
+    return ValueListenableBuilder(
       valueListenable: _isLoadingNotifier,
       builder: (BuildContext context, bool value, Widget? child) {
         if (value) return const SizedBox();
         return NormalButton(
-          title: 'Login',
+          title: 'login',
           onPressed: () async {
-            await _onPressed(context);
+            _isLoadingNotifier.value = true;
+            final response = await widget.onOperation.call();
+            if (response) {
+              Navigator.of(context).pop();
+            }
+            Future.delayed(const Duration(seconds: 2), () {
+              _isLoadingNotifier.value = false;
+            });
           },
         );
       },
